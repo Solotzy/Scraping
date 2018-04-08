8fd27ffdb49c6378130cd054019374fcb35518df
Solotzy <498442999@qq.com>
2018-04-08 17:56:41 +0800

    reindex.py

---
+++ ElasticSearch/etl.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+import requests # requests HTTP library
+import json # json parsing
+
+def extract():
+    f = open('tmdb.json')
+    if f:
+        return json.loads(f.read())
+
+def reindex(analysisSettings={}, mappingSettings={}, movieDict={}):
+    settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "index": {
+                "analysis": analysisSettings
+            }
+        }
+    }
+
+    if mappingSettings:
+        settings['mappings'] = mappingSettings
+    
+    resp = requests.delete("http://localhost:9200/tmdb")
+    resp = requests.put("http://localhost:9200/tmdb",
+                        data = json.dumps(settings))
+    
+    bulkMovies = ""
+    for id, movie in movieDict.iteritems():
+        addCmd = {"index": {"_index": "tmdb",
+                            "_type": "movie",
+                            "_id": movie["id"]}}
+        bulkMovies += json.dumps(addCmd) + "\n" + json.dumps(movie) + "\n"
+    
+    resp = requests.post("http://localhost:9200/_bulk", data=bulkMovies)
+
+movieDict = extract()
+reindex(movieDict=movieDict)
