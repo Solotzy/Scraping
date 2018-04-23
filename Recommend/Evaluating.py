a05d461b3c0de7ff0b6f3b0d80082e0647e0c850
Solotzy <498442999@qq.com>
2018-04-23 16:02:36 +0800

    popularity

--- Recommend/Evaluating.py
+++ Recommend/Evaluating.py
@@ -52,3 +52,21 @@ def Converage(train, test, N)
         for item, pui in rank:
             recommend_items.add(item)
     return len(recommend_items) / (len(all_items) * 1.0)
+
+# 评测指标 新颖度 如果推荐出的物品都很热门，说明推荐的新颖度低
+def Popularity(train, test, N):
+    item_popularity = dict()
+    for user, items in train.items()
+        for item in items().keys()
+            if item not in item_popularity:
+                item_popularity[item] = 0
+            item_popularity[item] += 1
+    ret = 0
+    n = 0
+    for user in train.keys():
+        rank = GetRecommendation(user, N)
+        for item, pui in rank:
+            ret += math.log(1 + item_popularity[item])
+            n += 1
+    ret /= n * 1.0
+    return ret
